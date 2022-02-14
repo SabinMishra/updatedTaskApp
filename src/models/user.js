c1ac04ac2dc79f626a5b9f6e24312fbd52ff94d7
Sabin Mishra <predictevsabin@gmail.com>
2022-02-14 20:58:21 +0545

    final

--- src/models/user.js
+++ src/models/user.js
@@ -109,7 +109,7 @@ userSchema.pre('save', async function (next) {
     next()
 })
 
-// Delete user tasks when user is removed
+
 userSchema.pre('remove', async function (next) {
     const user = this
     await Task.deleteMany({ owner: user._id })
